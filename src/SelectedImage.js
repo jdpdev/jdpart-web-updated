2e26affb3012fd641506705bf6d73b307b7020b2
gallahad@me.com <eYgbc6sc6GehM;XafYmUdUC#7mnUNGn>
2022-11-04 19:55:33 -0400

    show all images

--- src/SelectedImage.js
+++ src/SelectedImage.js
@@ -9,19 +9,40 @@ export default function SelectedImage() {
     const image = findImage(params.id);
 
     return (
-        <div className="image-container">
-            <div className="image">
-                <img src={image.images[0].url.full} alt={image.id} />
+        <>
+            <div className="image-container">
+                <div className="image">
+                    <img src={image.images[0].url.full} alt={image.id} />
+                </div>
+                <div className="details">
+                    <p>
+                        {image.description}
+                    </p>
+                    <p>
+                        {image.year}
+                    </p>
+                    {
+                        image.store &&
+                        <p>
+                            <a 
+                                href={image.store}
+                                target="_blank"
+                            >View in the store...</a>
+                        </p>
+                    }
+                </div>
             </div>
-            <div className="details">
-                <p>
-                    {image.description}
-                </p>
-                <p>
-                    {image.year}
-                </p>
-            </div>
-        </div>
+            {
+                image.images.length > 1 &&
+                <div className='extra-images-container'>
+                    { image.images.slice(1).map((img, i) => (
+                        <div>
+                            <img src={img.url.thumb} alt={image.id + "-" + i} />
+                        </div>
+                    )) }
+                </div>
+            }
+        </>
     )
 }
 
